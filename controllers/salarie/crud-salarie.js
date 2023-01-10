5b042af40cdc63e9a1a8555fc263811a51ed2d19
Abdelkrim <kissab@gmail.com>
2023-01-10 16:09:22 +0100

    commit

--- controllers/salarie/crud-salarie.js
+++ controllers/salarie/crud-salarie.js
@@ -1,7 +1,7 @@
 
 const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
-const SECRET = require("../../config.js")
+const SECRET = process.env.SECRET
 const pool = require("../../db");
 const express = require('express');
 const salarieRouter = express.Router();
