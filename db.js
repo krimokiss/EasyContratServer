5941350582d6d48571540afd57f5efc258905faa
Abdelkrim <kissab@gmail.com>
2023-01-10 10:40:24 +0100

    Render database

--- db.js
+++ db.js
@@ -1,11 +1,24 @@
 const Pool = require("pg").Pool;
 
+// const pool = new Pool({
+//     user: "postgres",
+//     password: "boubou",
+//     host: "localhost",
+//     port: 5432,
+//     database: "easycontrat"
+// });
+
+
+
 const pool = new Pool({
-    user: "postgres",
-    password: "boubou",
-    host: "localhost",
+    user: "krimo",
+    password: "NHmEY67R9P0BsQH8uotIjLm9zeYqvUvo",
+    host: "dpg-ceuil56n6mpglqd2v0ng-a.frankfurt-postgres.render.com",
     port: 5432,
-    database: "easycontrat"
+    database: "easycontrat",
+    ssl:true
 });
+
+
 
 module.exports = pool;
