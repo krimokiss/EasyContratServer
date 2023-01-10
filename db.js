ba409ebe6718e524c11930345fc113a1cb050c5d
Abdelkrim <kissab@gmail.com>
2023-01-10 11:40:16 +0100

    db.js

--- db.js
+++ db.js
@@ -11,12 +11,12 @@ const Pool = require("pg").Pool;
 
 
 const pool = new Pool({
-    user: "krimo",
-    password: "NHmEY67R9P0BsQH8uotIjLm9zeYqvUvo",
-    host: "dpg-ceuil56n6mpglqd2v0ng-a.frankfurt-postgres.render.com",
-    port: 5432,
-    database: "easycontrat",
-    ssl:true
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    host: process.env.HOST,
+    port: process.env.PORT,
+    database: process.env.DATABASE,
+    ssl:process.env.SSL
 });
 
 
