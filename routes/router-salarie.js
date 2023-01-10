ce3abfc8bca676a67802139868e7b36437025f78
Abdelkrim <kissab@gmail.com>
2023-01-10 14:45:42 +0100

    route login

--- routes/router-salarie.js
+++ routes/router-salarie.js
@@ -2,9 +2,13 @@ const express = require('express');
 const myRouter = express.Router();
 const salarie = require('../controllers/salarie/crud-salarie');
 const verifyToken = require('../middleware/auth.middleware')
+const {loginSalarie}=require('../controllers/salarie/crud-salarie')
+
+routeSalarie
+.post('/login', loginSalarie)
 
 
-myRouter.post('/login', salarie.loginSalarie)
+// myRouter.post('/login', salarie.loginSalarie)
 myRouter.post('/register', salarie.postUser )
 
 myRouter.use(verifyToken)
@@ -19,3 +23,4 @@ myRouter.get('/profil', salarie.getProfil)
 
 
 module.exports = myRouter
+module.exports = routeSalarie
