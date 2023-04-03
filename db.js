e6e41ce7763eae2f549b76c0b5e4d5037964e6a3
unknown <kissab@gmail.com>
2023-04-03 17:03:23 +0200

    db to local

--- db.js
+++ db.js
@@ -1,24 +1,24 @@
 const Pool = require("pg").Pool;
 // require("dotenv").config()
 
-// const pool = new Pool({
-//     user: "postgres",
-//     password: "boubou",
-//     host: "localhost",
-//     port: 5432,
-//     database: "easycontrat"
-// });
-
-
-
 const pool = new Pool({
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    host: "dpg-ceuil56n6mpglqd2v0ng-a.frankfurt-postgres.render.com",
+    user: "postgres",
+    password: "boubou",
+    host: "localhost",
     port: 5432,
-    database: process.env.DATABASE,
-    ssl:true
+    database: "easycontrat"
 });
+
+
+
+// const pool = new Pool({
+//     user: process.env.USER,
+//     password: process.env.PASSWORD,
+//     host: "dpg-ceuil56n6mpglqd2v0ng-a.frankfurt-postgres.render.com",
+//     port: 5432,
+//     database: process.env.DATABASE,
+//     ssl:true
+// });
 
 
 
--- routes/router-entreprise.js
+++ routes/router-entreprise.js
@@ -2,6 +2,18 @@ const express = require('express');
 const myRouter = express.Router();
 const entreprise = require('../controllers/entreprise/crud-entreprise');
 const verifyToken = require('../middleware/auth.middleware')
+const rateLimit = require("express-rate-limit");
+
+// Configuration de la limite de tentatives de connexion
+const loginLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 5, // limite de 5 tentatives de connexion par fenêtre
+    handler: (req, res, next) => {
+        res.status(429).json({
+          message: "Vous avez dépassé la limite de tentatives de connexion. Veuillez réessayer dans 15 minutes."
+        });
+      },
+  });
 
 
 myRouter.post('/login', entreprise.loginEntreprise)
